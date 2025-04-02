bc1ec745a6dc49ce0fdde250ee71cf56f572499f
Xander Wauters <xwauters@outlook.com>
2025-04-02 11:54:58 +0200

    UPDATE: Disable logging propagation for pika and clear logfile at the start of user creation listener

--- user-creation-listener/user_creation_listener.py
+++ user-creation-listener/user_creation_listener.py
@@ -12,6 +12,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("pika").propagate = False
 open('logfile.log', 'w').close()
 
 # Database connection
