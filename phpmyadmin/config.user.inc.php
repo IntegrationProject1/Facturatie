1d1b4be70e546e54c420831b580a2c629da4970e
Xander Wauters <xwauters@outlook.com>
2025-05-06 14:17:29 +0200

    Add HTTPS redirection in phpMyAdmin configuration

--- phpmyadmin/config.user.inc.php
+++ phpmyadmin/config.user.inc.php
@@ -1,6 +1,11 @@
 <?php
+
+if (empty($_SERVER['HTTPS'])) {
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+}
+
 $cfg["ForceSSL"] = true;
 $cfg["Servers"][1]["ssl"] = true;
 $cfg["Servers"][1]["ssl_key"] = "/etc/ssl/private/key.pem";
 $cfg["Servers"][1]["ssl_cert"] = "/etc/ssl/certs/cert.pem";
-?>
